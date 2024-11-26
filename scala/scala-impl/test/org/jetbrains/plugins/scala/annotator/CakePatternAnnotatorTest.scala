73e227767703295f245e2b9243d60f19ac7ad18f
Jason Zaugg <jzaugg@gmail.com>
2024-11-26 20:00:15 +1000

    Add an ignored test for the last known major issue in Scalac highlighting

--- scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/CakePatternAnnotatorTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/CakePatternAnnotatorTest.scala
@@ -1,10 +1,10 @@
-package org.jetbrains.plugins.scala.lang.resolve
+package org.jetbrains.plugins.scala.annotator
 
 import org.intellij.lang.annotations.Language
-import org.jetbrains.plugins.scala.annotator.{AnnotatorHolderMock, AnnotatorSimpleTestCase, Message, ScalaAnnotator}
+import org.jetbrains.plugins.scala.ScalaVersion
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.extensions._
-import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScExpression, ScMethodCall}
 
 class CakePatternAnnotatorTest extends AnnotatorSimpleTestCase {
 
@@ -43,11 +43,73 @@ class CakePatternAnnotatorTest extends AnnotatorSimpleTestCase {
          |    val tree: RefChecks.this.global.gen.global.Tree
          |    val block: RefChecks.this.global.gen.global.Block
          |    def testMe(): Unit = {
-         |//      global.useTree(tree) // OK
-         |      global.useTree(block) // NOK
+         |      global.useTree(tree) // OK
+         |      global.useTree(block) // was NOK
          |    }
          |  }
          |}
+         |""".stripMargin)) {
+      case Nil =>
+    }
+
+  // TODO The source code in the test has error annotations when I view it in the IDE but they don't
+  //      appear in this test. Why?
+  // TODO Fix the actual problem that is typeing the qualifier `global.gen.C` in the pattern match
+  //      with an unstable type.
+  def ignoreCakeyPatterns(): Unit =
+    assertMatches(messages(
+      s"""
+         |
+         |abstract class ApiUniverse extends ApiTrees {
+         |  def useTree(t: Tree) = ()
+         |}
+         |
+         |trait ApiTrees {
+         |  self: ApiUniverse =>
+         |  type Tree
+         |}
+         |
+         |abstract class ApiTreeGen {
+         |  val global: ApiUniverse
+         |  case class C(t: global.Tree)
+         |  object D {
+         |    def unapply(a: Any): Option[global.Tree] = null
+         |  }
+         |}
+         |
+         |abstract class Global extends ApiUniverse {
+         |  object gen extends { val global: Global.this.type = Global.this } with ApiTreeGen {}
+         |
+         |  new Object match {
+         |    case gen.C(t) => useTree(t) // OK
+         |  }
+         |}
+         |
+         |abstract class RefChecks {
+         |  val global: Global
+         |
+         |  def test = {
+         |    val C2 = global.gen.C
+         |
+         |    new Object match {
+         |      case global.gen.C(t) => global.useTree(t) // NOK
+         |      case global.gen.D(t) => global.useTree(t) // NOK
+         |      case C2(t) => global.useTree(t) // OK
+         |    }
+         |
+         |    /* SCALAC:
+         |      val C2: RefChecks.this.global.gen.C.type = RefChecks.this.global.gen.C;
+         |      new java.lang.Object() match {
+         |        case (t: RefChecks.this.global.gen.global.Tree): RefChecks.this.global.gen.C((t @ _)) => RefChecks.this.global.useTree(t)
+         |        case RefChecks.this.global.gen.D.unapply(<unapply-selector>) <unapply> ((t @ _)) => RefChecks.this.global.useTree(t)
+         |        case (t: RefChecks.this.global.gen.global.Tree): RefChecks.this.global.gen.C((t @ _)) => RefChecks.this.global.useTree(t)
+         |      }
+         |    }
+         |     */
+         |  }
+         |}
+         |
+         |
          |""".stripMargin)) {
       case Nil =>
     }
@@ -59,7 +121,11 @@ class CakePatternAnnotatorTest extends AnnotatorSimpleTestCase {
     implicit val mock: AnnotatorHolderMock = new AnnotatorHolderMock(parse)
 
     parse.depthFirst().filterByType[ScExpression].foreach {
-      annotator.annotate(_, typeAware = true)
+      case t: ScMethodCall =>
+        val text = t.getText
+        annotator.annotate(t, typeAware = true)
+      case t =>
+        annotator.annotate(t, typeAware = true)
     }
 
     mock.annotations
